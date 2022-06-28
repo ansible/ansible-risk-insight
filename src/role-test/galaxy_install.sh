c87fb8cc0805562038acdfaeb30eb7129e2be7b9
Riko Kudo <rurikudo@ibm.com>
2022-06-28 15:20:42 +0900

    add splitted role list
    Signed-off-by: Riko Kudo <rurikudo@ibm.com>

--- src/role-test/galaxy_install.sh
+++ src/role-test/galaxy_install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+INPUT_FILE=$1
+TEST_DATA_DIR=$2
+
 while read line
 do
   echo $line
   ROLE=$line
   ROLE_DIR=${ROLE//./-}
-  ansible-galaxy install $ROLE -p ./testdata/$ROLE_DIR
-done < ./role-list.txt
+  ansible-galaxy install $ROLE -p $TEST_DATA_DIR/$ROLE_DIR
+done < $INPUT_FILE
