41c1d0b49cd90a7faf87d3c0710cc4e6ec988b13
Hirokuni-Kitahara1 <hirokuni.kitahara1@ibm.com>
2022-08-08 20:12:46 +0900

    update converter1
    Signed-off-by: Hirokuni-Kitahara1 <hirokuni.kitahara1@ibm.com>

--- src/converter1.py
+++ src/converter1.py
@@ -1,19 +1,14 @@
 import argparse
-# from logging import root
 import os
+import sys
 import json
 import jsonpickle
-from tree import TreeNode, key_to_file_name, load_node_objects, detect_type, TreeLoader
+import logging
+from struct5 import ObjectList, detect_type, ExecutableType
+from tree import TreeNode, key_to_file_name, load_node_objects, TreeLoader, TreeNode, load_all_definitions
 from context import Context, resolve_module_options
 
-def convert(root_key, data_dir):
-
-    tree_file = os.path.join(data_dir, key_to_file_name("tree", root_key))
-    node_file = os.path.join(data_dir, key_to_file_name("node", root_key))
-
-    tree = TreeNode.load(tree_file)
-    node_objects = load_node_objects(node_file)
-
+def convert(tree, node_objects):
     node_dict = {}
     for no in node_objects.items:
         node_dict[no.key] = no
@@ -26,25 +21,17 @@ def convert(root_key, data_dir):
         # define here
         obj["type"] = node_type
 
-        if node_type == "Module":
+        if node_type == "module":
             obj["fqcn"] = no.fqcn
         else:
-            # obj["key"] = no["key"]
             if "name" in obj:
                 obj["name"] = no.name
 
-            if node_type == "Task":
+            if node_type == "task":
                 obj["name"] = no.name
-                # obj["role"] = no["role"]
-                # obj["collection"] = no["collection"]
-                # obj["definition"] = no
 
-                if no.executable_type == "Module":
-                    # obj["executable_type"] = no["executable_type"]
+                if no.executable_type == ExecutableType.MODULE_TYPE:
                     obj["resolved_name"] = no.resolved_name
-                # obj["definition"] = no
-            # else:
-            #     obj["definition"] = no
 
             children_per_type = {}
             for c in node.children:
@@ -55,23 +42,26 @@ def convert(root_key, data_dir):
                     children_per_type[ctype] = [c]
 
             #obj["children_types"] = list(children_per_type.keys())
-            if "Playbook" in children_per_type:
-                obj["playbooks"] = [ getSubTree(c) for c in children_per_type["Playbook"] ]
-            if "Play" in children_per_type:
-                obj["plays"] = [ getSubTree(c) for c in children_per_type["Play"] ]
-            if "Role" in children_per_type:
-                obj["roles"] = [ getSubTree(c) for c in children_per_type["Role"]]
-            if "TaskFile" in children_per_type:
-                obj["taskfiles"] = [ getSubTree(c) for c in children_per_type["TaskFile"]]
-            if "Task" in children_per_type:
-                obj["tasks"] = [ getSubTree(c) for c in children_per_type["Task"]]
+            if "playbook" in children_per_type:
+                obj["playbooks"] = [ getSubTree(c) for c in children_per_type["playbook"] ]
+            if "play" in children_per_type:
+                obj["plays"] = [ getSubTree(c) for c in children_per_type["play"] ]
+            if "role" in children_per_type:
+                obj["roles"] = [ getSubTree(c) for c in children_per_type["role"]]
+            if "taskfile" in children_per_type:
+                obj["taskfiles"] = [ getSubTree(c) for c in children_per_type["taskfile"]]
+            if "task" in children_per_type:
+                obj["tasks"] = [ getSubTree(c) for c in children_per_type["task"]]
+            if "module" in children_per_type:
+                tmp_children = [getSubTree(c) for c in children_per_type["module"]]
+                obj["resolved_name"] = tmp_children[0]["fqcn"] if len(tmp_children) > 0 else ""
         # end
         return obj
 
     tObj = getSubTree(tree)
     tObj["dependent_collections"] = list(set([ no.collection for no in node_objects.items if hasattr(no, "collection") and no.collection != ""]))
     tObj["dependent_roles"] = list(set([ no.role for no in node_objects.items if hasattr(no, "collection") and hasattr(no, "role") and no.collection == "" and no.role != ""]))
-    tObj["dependent_module_collections"] = list(set([ no.collection for no in node_objects.items if detect_type(no.key)=="Module" and hasattr(no, "collection") and no.collection != ""]))
+    tObj["dependent_module_collections"] = list(set([ no.collection for no in node_objects.items if detect_type(no.key)=="module" and hasattr(no, "collection") and no.collection != ""]))
     # tObj["dependent_module_roles"] = list(set([ no["role"] for no in node_objects if detect_type(no["key"])=="Module" and "collection" not in no]))
 
     context_and_task = []
@@ -104,6 +94,28 @@ def convert(root_key, data_dir):
 
     return tObj, contexts
 
+def load_tree_json(tree_path):
+    trees = []
+    with open(tree_path, "r") as file:
+        for line in file:
+            d = json.loads(line)
+            src_dst_array = d.get("tree", [])
+            tree = TreeNode.load(graph=src_dst_array)
+            trees.append(tree)
+    return trees
+
+def load_node_objects(node_path="", root_dir="", ext_dir=""):
+    objects = ObjectList()
+    if node_path != "":
+        objects.from_json(fpath=node_path)
+    else:
+        root_defs = load_all_definitions(root_dir)
+        ext_defs = load_all_definitions(ext_dir)
+        for type_key in root_defs:
+            objects.merge(root_defs[type_key])
+            objects.merge(ext_defs[type_key])
+    return objects
+
 def main():
     parser = argparse.ArgumentParser(
         prog='converter1.py',
@@ -112,26 +124,28 @@ def main():
         add_help=True,
     )
 
-    parser.add_argument('-t', '--target', default="", help='target key which is the root (or leaf if reverse) of the tree')
-    parser.add_argument('-d', '--dir', default="", help='path to input directory')
-    parser.add_argument('-g', '--graph', default="graph.json", help='path to the graph file')
+    parser.add_argument('-t', '--tree-file', default="", help='path to tree json file')
+    parser.add_argument('-n', '--node-file', default="", help='path to node object json file')
+    parser.add_argument('-r', '--root-dir', default="", help='path to definitions dir for root')
+    parser.add_argument('-e', '--ext-dir', default="", help='path to definitions dir for ext')
 
     args = parser.parse_args()
 
-    data_dir = args.dir
-    if args.target != "":
-        root_key = args.target
-        tObj = convert(root_key, data_dir)
-        print(json.dumps(tObj, indent=2))
-    elif args.graph != "":
-        t = TreeLoader(args.graph)
-        playbook_keys = t.find_playbooks()
-        for k in playbook_keys:
-            tObj = convert(k, data_dir)
-            print(json.dumps(tObj, indent=2))
-    else:
-        raise ValueError("target or graph must be specified to show tree(s)")
+    if args.tree_file == "":
+        logging.error("\"--tree-file\" is required")
+        sys.exit(1)
 
+    if args.node_file == "" and (args.root_dir == "" or args.ext_dir == ""):
+        logging.error("\"--root-dir\" and \"--ext-dir\" are required when \"--node-file\" is empty")
+        sys.exit(1)
+
+    trees = load_tree_json(args.tree_file)
+    objects = load_node_objects(args.node_file, args.root_dir, args.ext_dir)
+
+    for tree in trees:
+        t_obj = convert(tree, objects)
+        print(json.dumps(t_obj, indent=2))
+        break
 
 if __name__ == "__main__":
     main()
