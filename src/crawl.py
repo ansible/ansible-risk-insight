50afb4a4b7ed557cf9867306c15cf6944ae43c73
Yuji Watanabe <muew@jp.ibm.com>
2022-08-15 13:50:40 +0900

    fix target name for collection as root

--- src/crawl.py
+++ src/crawl.py
@@ -1,15 +1,8 @@
-from dataclasses import dataclass, field
 import argparse
 import os
 import sys
-import pathlib
 import json
-import jsonpickle
-import yaml
 import logging
-import git
-import datetime
-import joblib
 import subprocess
 import tempfile
 import shutil
@@ -17,10 +10,8 @@ import copy
 
 from subprocess import PIPE
 
-from resolver_fqcn import FQCNResolver
 from struct5 import Load, LoadType
-from safe_glob import safe_glob
-from loader import detect_target_type, supported_target_types, get_loader_version, create_load_json_path
+from loader import detect_target_type, supported_target_types, get_loader_version, create_load_json_path, get_target_name
 from parser import load_name2target_name, Parser
 
 def crawl(is_ext, output_path, index_path, crawl_target_path):
@@ -43,11 +34,11 @@ def crawl(is_ext, output_path, index_path, crawl_target_path):
 
     for i, (target_path) in enumerate(profiles):
 
-        load_json_path = output_path
-        target_name = target_path
-
+        target_name = get_target_name(target_type, target_path)
         if is_ext:
-            load_json_path, target_name = create_load_json_path(target_type, target_path, output_path)
+            load_json_path = create_load_json_path(target_type, target_name, output_path)
+        else:
+            load_json_path = output_path
 
         print("[{}/{}] {} {}       ".format(i+1, num, target_type, target_name))
 
@@ -78,7 +69,9 @@ def crawl(is_ext, output_path, index_path, crawl_target_path):
         if is_ext:
 
             for target_path in profiles:
-                load_json_path, target_name = create_load_json_path(target_type, target_path, output_path)
+                target_name = get_target_name(target_type, target_path)
+                load_json_path = create_load_json_path(target_type, target_name, output_path)
+                target_name = get_target_name(target_type, target_path)
                 lf = load_json_path.replace(output_path, "")
                 if lf.startswith("/"):
                     lf = lf[1:]
@@ -158,8 +151,9 @@ def move_index(path1, path2, params):
     with open(path1, "r") as f1:
         js1 = json.load(f1)
         js2 = copy.deepcopy(js1)
-        for p in params:
-            js2[p] = params[p]
+        if any(params):
+            for p in params:
+                js2[p] = params[p]
         with open(path2, "w") as f2:
             json.dump(js2, f2)
     return js1, js2
@@ -189,21 +183,18 @@ def move_load_file(path1, src1, path2, src2):
     with open(path2, "w") as f2:
         json.dump(js2, f2)
 
-def move_definitions(dir1, src1, dir2, src2, name, ttype):
+def move_definitions(dir1, src1, dir2, src2):
 
     if not os.path.exists(dir2):
         os.makedirs(dir2)
     if not os.path.isdir(dir2):
         raise ValueError("{} is not directory".format(dir2))
-    if ttype == "" or name == "":
-        raise ValueError("invalid name or type: {}, {}".format(name, ttype))
 
-    def1 = os.path.join(dir1, "{}-{}".format(ttype, name))
-    if not os.path.exists(def1) or not os.path.isdir(def1):
-        raise ValueError("{} is invalid definition directory".format(def1))
+    if not os.path.exists(dir1) or not os.path.isdir(dir1):
+        raise ValueError("{} is invalid definition directory".format(dir1))
 
     js2 = None
-    map1 = os.path.join(def1, "mappings.json")
+    map1 = os.path.join(dir1, "mappings.json")
     with open(map1, "r") as f1:
         js1 = json.load(f1)
         js2 = copy.deepcopy(js1)
@@ -211,45 +202,16 @@ def move_definitions(dir1, src1, dir2, src2, name, ttype):
         p2 = p1.replace(src1, src2, 1)
         js2["path"] = p2
 
-    def2 = os.path.join(dir2, "{}-{}".format(ttype, name))
-    if os.path.exists(def2):
-        shutil.rmtree(def2)
-    shutil.copytree(def1, def2, dirs_exist_ok=True)
-    map2 = os.path.join(def2, "mappings.json")
+    if os.path.exists(dir2):
+        shutil.rmtree(dir2)
+    shutil.copytree(dir1, dir2, dirs_exist_ok=True)
+    map2 = os.path.join(dir2, "mappings.json")
     with open(map2, "w") as f2:
         json.dump(js2, f2)
 
+def crawl_ext(_target, _target_type, _common_data_dir):
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(
-        prog='crawl.py',
-        description='crawl a collection and store data under a common directory',
-        epilog='end',
-        add_help=True,
-    )
-
-    parser.add_argument('-c', '--collection', default="", help='collection name')
-    parser.add_argument('-o', '--output-dir', default="", help='path to the output directory')
-
-    args = parser.parse_args()
-
-    if not args.collection:
-        logging.error("collection must be specified")
-        sys.exit(1)
-
-    if not args.output_dir:
-        logging.error("output dir must be specified")
-        sys.exit(1)
-
-    _is_ext = True
-    _target = args.collection
-    _target_type = "collection"
-    _common_data_dir = args.output_dir
-
-    # python crawl.py --ext -t $TRIAL_DIR/src -o /tmp/aaaa -i /tmp/index-aaaa/index-ext.json
-    # python parser.py --ext -i /tmp/index-aaaa/index-ext.json -o /tmp/bbbb
-
+    is_ext = True
     _common_src_dir = os.path.join(_common_data_dir, "src")
     _common_ext_dir = os.path.join(_common_data_dir, "ext")
     _common_def_dir = os.path.join(_common_data_dir, "definitions")
@@ -275,7 +237,6 @@ if __name__ == "__main__":
         # _crawl_target_path = "/tmp/collections/debops.debops/src"
         _src_dir = os.path.join(dname, "src")
         _crawl_target_path = _src_dir
-        _load_path = ""
 
         print("temporary dir {} created".format(dname))
 
@@ -304,11 +265,11 @@ if __name__ == "__main__":
         # ansible-galaxy collection install
         # load src, create load.json
         print("loading collections")
-        crawl(_is_ext, _load_output_path, _load_index_path, _crawl_target_path)
+        crawl(is_ext, _load_output_path, _load_index_path, _crawl_target_path)
 
         # load index_
         print("decomposing files")
-        load_definitions(_is_ext, _load_index_path, _load_path, _defs_out_path)
+        load_definitions(is_ext, _load_index_path, "", _defs_out_path)
 
         # move the files to common dir
 
@@ -324,7 +285,6 @@ if __name__ == "__main__":
         print("moving index")
         lidx1, lidx2 = move_index(_load_index_path, _new_load_index_path, params)
 
-
         print("moving results to common dir")
         out_path = lidx1.get("out_path","")
         generated_load_files = lidx1.get("generated_load_files",[])
@@ -335,7 +295,9 @@ if __name__ == "__main__":
             load_file_path = os.path.join(out_path, load_file)
             new_load_file_path = os.path.join(_common_ext_dir, load_file)
             move_load_file(load_file_path, _src_dir, new_load_file_path, _common_src_dir)
-            move_definitions(_defs_out_path, _src_dir, _common_def_dir, _common_src_dir, target_name, target_type)
+            def1 = os.path.join(_defs_out_path, "{}-{}".format(target_type, target_name))
+            def2 = os.path.join(_common_def_dir, "{}-{}".format(target_type, target_name))
+            move_definitions(def1, _src_dir, def2, _common_src_dir)
 
         shutil.move(_install_log_path, _new_log_path)
 
@@ -358,80 +320,120 @@ if __name__ == "__main__":
     else:
         print("temporary dir {} remaining somehow...".format(dname))
 
-    # parser = argparse.ArgumentParser(
-    #     prog='loader.py',
-    #     description='load project/collection(s)/role(s)/playbook and make a load json',
-    #     epilog='end',
-    #     add_help=True,
-    # )
 
-    # parser.add_argument('-t', '--target-path', default="", help='target path')
-    # parser.add_argument('-o', '--output-path', default="", help='path to output dir/file')
-    # parser.add_argument('-i', '--index-path', default="", help='path to the output index.json')
-    # parser.add_argument('--root', action='store_true', help='enable this if the target is the root')
-    # parser.add_argument('--ext', action='store_true', help='enable this if the target is the external dependency(s)')
+def crawl_root(target, target_type, common_data_dir):
 
-    # args = parser.parse_args()
+    is_ext = False
+    src_index_file = os.path.join(common_data_dir, "{}-{}-index-ext.json".format(target_type, target))
 
-    # if not args.root and not args.ext:
-    #     logging.error("either \"--root\" or \"--ext\" must be specified")
-    #     sys.exit(1)
-
-    # if args.root and not args.output_path.endswith(".json"):
-    #     logging.error("\"--output-path\" must be a single .json file for \"--root\" mode")
-    #     sys.exit(1)
-
-    # is_ext = args.ext
-    # output_path = args.output_path
-    # index_path = args.index_path
-    # crawl_target_path = args.target_path
-
-    # crawl(is_ext, output_path, index_path, crawl_target_path)
-
-    # parser = argparse.ArgumentParser(
-    #     prog='parser.py',
-    #     description='parse collection/role and its children and output definition json',
-    #     epilog='end',
-    #     add_help=True,
-    # )
-
-    # parser.add_argument('-l', '--load-path', default="", help='load json path/dir')
-    # parser.add_argument('-i', '--index-path', default="", help='if specified, load files in this index.json (--load-path will be ignored)')
-    # parser.add_argument('--root', action='store_true', help='enable this if the target is the root')
-    # parser.add_argument('--ext', action='store_true', help='enable this if the target is the external dependency(s)')
-    # parser.add_argument('-o', '--output-dir', default="", help='path to the output dir')
-
-    # args = parser.parse_args()
-
-    # if not args.root and not args.ext:
-    #     logging.error("either \"--root\" or \"--ext\" must be specified")
-    #     sys.exit(1)
-    # is_ext = args.ext
-
-    # if args.load_path == "" and args.index_path == "":
-    #     logging.error("either `--load-path` or `--index-path` is required")
-    #     sys.exit(1)
-
-    # if args.root and args.load_path == "":
-    #     logging.error("\"--load-path\" must be specified for \"--root\" mode")
-    #     sys.exit(1)
-
-    # if args.root and not os.path.isfile(args.load_path):
-    #     logging.error("\"--load-path\" must be a single .json file for \"--root\" mode")
-    #     sys.exit(1)
-
-    # if args.load_path != "" and not os.path.exists(args.load_path):
-    #     logging.error("No such file or directory: {}".format(args.load_path))
-    #     sys.exit(1)
-
-    # if args.index_path != "" and not os.path.exists(args.index_path):
-    #     logging.error("No such file or directory: {}".format(args.index_path))
-    #     sys.exit(1)
-
-    # index_path = args.index_path
-    # load_path = args.load_path
-    # output_path = args.output_dir
-
-    # load_definitions(index_path, load_path, output_path)
+    with open(src_index_file, "r") as f:
+        src_index = json.load(f)
+        src_load_dir = src_index.get("out_path", "")
+        for v in src_index.get("generated_load_files", []):
+            if v["name"] == target and v["type"] == target_type:
+                src_load_file = os.path.join(src_load_dir, v["file"])
 
 
+    with open(src_load_file, "r") as f:
+        src_load = json.load(f)
+        src_dir = src_load.get("path", "")
+
+    # dst_dir = "/tmp/tehe-dst"
+    dst_dir = os.path.join(common_data_dir, "root")
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+
+        tmp_load_file = os.path.join(tmpdir, "load-root.json")
+        tmp_defs_dir = os.path.join(tmpdir, "definitions")
+
+        print("temporary dir {} created".format(tmpdir))
+
+        _params_before = {
+            "is_ext": is_ext,
+            "target": target,
+            "target_type": target_type,
+            "tmp_load_file": tmp_load_file,
+            "tmp_defs_dir" : tmp_defs_dir,
+            "src_dir": src_dir
+        }
+
+        print("before move")
+        print(json.dumps(_params_before, indent=2))
+
+        # ansible-galaxy collection install
+        # load src, create load.json
+        print("loading collections")
+        crawl(is_ext, tmp_load_file, "", src_dir)
+
+        # load index_
+        print("decomposing files")
+        load_definitions(is_ext, "", tmp_load_file, tmp_defs_dir)
+
+        # move the files to common dir
+
+        dst_load_file = os.path.join(dst_dir, "{}-{}-load-root.json".format(target_type, target))
+
+        print("moving load file")
+        move_index(tmp_load_file, dst_load_file, {})
+
+        print("moving results to common dir")
+        dst_def_dir = os.path.join(dst_dir, "definitions", target_type, target)
+        if not os.path.exists(dst_def_dir):
+            os.makedirs(dst_def_dir)
+
+        move_definitions(tmp_defs_dir, src_dir, dst_def_dir, src_dir)
+
+        _params_after = {
+            "_is_ext": is_ext,
+            "_target": target,
+            "_target_type": target_type,
+            "_load_output_path": dst_load_file,
+            "_defs_out_path" : dst_def_dir,
+            "_src_dir": src_dir
+        }
+
+        print("after move")
+        print(json.dumps(_params_after, indent=2))
+
+    if not os.path.exists(tmpdir):
+        print("temporary dir {} cleared".format(tmpdir))
+    else:
+        print("temporary dir {} remaining somehow...".format(tmpdir))
+
+def tree():
+    return
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        prog='crawl.py',
+        description='crawl a collection and store data under a common directory',
+        epilog='end',
+        add_help=True,
+    )
+
+    parser.add_argument('-c', '--collection', default="", help='collection name')
+    parser.add_argument('-o', '--output-dir', default="", help='path to the output directory')
+
+    args = parser.parse_args()
+
+    if not args.collection:
+        logging.error("collection must be specified")
+        sys.exit(1)
+
+    if not args.output_dir:
+        logging.error("output dir must be specified")
+        sys.exit(1)
+
+    _is_ext = True
+    _target = args.collection
+    _target_type = "collection"
+    _common_data_dir = args.output_dir
+
+    crawl_ext( _target, _target_type, _common_data_dir)
+    crawl_root(_target, _target_type, _common_data_dir)
+    tree()
