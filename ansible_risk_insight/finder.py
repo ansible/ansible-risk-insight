fe74c478b21fd4f5705f201f6f618299a62faed0
Yuji Watanabe <muew@jp.ibm.com>
2022-09-19 18:18:05 +0900

    enable flake8 & black  (#14)
    * enable flake8 and black
    
    Signed-off-by: Yuji Watanabe <muew@jp.ibm.com>
    
    * flake8 & black added as pre-commit tasks
    
    Signed-off-by: Yuji Watanabe <muew@jp.ibm.com>
    
    Signed-off-by: Yuji Watanabe <muew@jp.ibm.com>

--- ansible_risk_insight/finder.py
+++ ansible_risk_insight/finder.py
@@ -37,6 +37,7 @@ class TaskKeywordSet(metaclass=Singleton):
 class BuiltinModuleSet(metaclass=Singleton):
     builtin_modules: set
 
+
 p = Path(__file__).resolve().parent
 with open(p / "task_keywords.txt", "r") as f:
     TaskKeywordSet(set(f.read().splitlines()))
@@ -75,10 +76,7 @@ def get_task_blocks(fpath="", task_dict_list=None):
             try:
                 d = yaml.safe_load(file)
             except Exception as e:
-                logging.error(
-                    "failed to load this yaml file to get task blocks; {}"
-                    .format(e.args[0])
-                )
+                logging.error("failed to load this yaml file to get task blocks; {}".format(e.args[0]))
                 return None
     elif task_dict_list is not None:
         d = task_dict_list
@@ -152,9 +150,7 @@ def search_taskfiles_for_playbooks(path, taskfile_dir_paths=[]):
     # must copy the input here; otherwise, the added items are kept forever
     search_targets = [p for p in taskfile_dir_paths]
     for playbook_taskfile_dir_pattern in playbook_taskfile_dir_patterns:
-        search_targets.append(
-            os.path.join(path, playbook_taskfile_dir_pattern)
-        )
+        search_targets.append(os.path.join(path, playbook_taskfile_dir_pattern))
     candidates = []
     for search_target in search_targets:
         patterns = [search_target + "/**/*.yml", search_target + "/**/*.yaml"]
@@ -171,10 +167,7 @@ def search_taskfiles_for_playbooks(path, taskfile_dir_paths=[]):
                 try:
                     d = yaml.safe_load(file)
                 except Exception as e:
-                    logging.error(
-                        "failed to load this yaml file to search task"
-                        " files; {}".format(e.args[0])
-                    )
+                    logging.error("failed to load this yaml file to search task" " files; {}".format(e.args[0]))
             # if d cannot be loaded as tasks yaml file, skip it
             if d is None or not isinstance(d, list):
                 continue
@@ -235,10 +228,7 @@ def find_collection_name_of_repo(path):
             try:
                 my_collection_info = yaml.safe_load(file)
             except Exception as e:
-                logging.error(
-                    "failed to load this yaml file to read galaxy.yml; {}"
-                    .format(e.args[0])
-                )
+                logging.error("failed to load this yaml file to read galaxy.yml; {}".format(e.args[0]))
         if my_collection_info is None:
             return ""
         namespace = my_collection_info.get("namespace", "")
